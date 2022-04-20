a1755f1336f6a0dc501d9ae1a28f81a4feec3dae
Essmallwood <EsaylorSmallwood@wil.yearup.org>
2022-04-19 22:50:47 -0400

    Phone Book HW

--- src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
+++ src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
@@ -1,54 +1,85 @@
 package com.zipcodewilmington.phonebook;
 
+
+
 import java.util.List;
-import java.util.ArrayList;
-//import java.util.HashMap;
-import java.util.Iterator;
-import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.*;
 
-/**
- * Created by leon on 1/23/18.
- * Made WAY better by kristofer 6/16/20
- */
-public class PhoneBook {
 
-    private final Map<String, List<String>> phonebook;
+    public class PhoneBook {
+        private Map<String, List<String>> phonebook;
 
-    public PhoneBook(Map<String, List<String>> map) {
-        this.phonebook = null;
-    }
+        public PhoneBook(){
+         //this.phonebook = phonebook <>
+         phonebook = new HashMap<>();
+        }
+        public PhoneBook(Map<String, List<String>> map22) {
+            phonebook = new HashMap<>();
 
-    public PhoneBook() {
-        this(null);
-    }
+        }
 
-    public void add(String name, String phoneNumber) {
-    }
+        public void add(String name, String phoneNumber) {
+            // Getting the list view of Array numbers
+            ArrayList<String> num = new ArrayList<>(Arrays.asList(phoneNumber));
+          // phonebook.put(phoneNumber);
+            phonebook.put(name,num);
+        }
 
-    public void addAll(String name, String... phoneNumbers) {
-    }
+//
+        public void addAll(String name, String[] phoneNumbers) {
+            //ArrayList<String> num = new ArrayList<>(
+             phonebook.put( name, Arrays.asList(phoneNumbers));      //Arrays.asList(phoneNumbers);
 
-    public void remove(String name) {
-    }
+      ;
+        }
 
-    public Boolean hasEntry(String name) {
-        return null;
-    }
+        public void remove(String name) {
+            phonebook.remove(name);
+        }
 
-    public List<String> lookup(String name) {
-        return null;
-    }
+        public Boolean hasEntry(String name) {
 
-    public String reverseLookup(String phoneNumber)  {
-        return null;
-    }
+        if (phonebook.containsKey(name)) {
+            return true;
+        }
+        else {
+            return false;
+        }
+        }
 
-    public List<String> getAllContactNames() {
-        return null;
-    }
+        public List<String> lookup(String name) {
+            List<String> numbers = phonebook.get(name);
+            return numbers;
 
-    public Map<String, List<String>> getMap() {
-        return null;
-    }
-}
+
+        }
+
+        public String reverseLookup(String phoneNumber) {
+            for (Map.Entry<String, List<String>> entry : phonebook.entrySet()) {
+                if (entry.getValue().contains(phoneNumber)) {
+
+                    return entry.getKey();
+                }
+            }
+           return null;
+
+
+      }
+
+        public List<String> getAllContactNames() {
+
+            ArrayList<String> names = new ArrayList<>();
+            for (String name : this.phonebook.keySet()) {
+                names.add(name);
+            }
+            return names;
+        }
+
+        public Map<String, List<String>> getMap() {
+
+            return this.phonebook;
+       }
+
+ }
+
--- src/test/java/com/zipcodewilmington/phonebook/GetAllContactNames.java
+++ src/test/java/com/zipcodewilmington/phonebook/GetAllContactNames.java
@@ -1,60 +1,62 @@
 package com.zipcodewilmington.phonebook;
 
+
 import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.Arrays;
 import java.util.List;
 
-public class GetAllContactNames {
-    @Test
-    public void test1() {
-        // given
-        PhoneBook phoneBook = new PhoneBook();
-        String[] names = new String[]{"John", "Joe", "Jim", "Jay"};
-        for (int i = 0; i < names.length; i++) {
-            String name = names[i];
-            phoneBook.add(name, "");
+    public class GetAllContactNames {
+        @Test
+        public void test1() {
+            // given
+            PhoneBook phoneBook = new PhoneBook();
+            String[] names = new String[]{"Joe", "Jay", "John", "Jim"};
+            for (int i = 0; i < names.length; i++) {
+                String name = names[i];
+                phoneBook.add(name, "");
+            }
+
+            // when
+            List<String> actualNames = phoneBook.getAllContactNames();
+
+            // then
+            Assert.assertEquals(Arrays.asList(names), actualNames);
         }
 
-        // when
-        List<String> actualNames = phoneBook.getAllContactNames();
+        @Test
+        public void test2() {
+            // given
+            PhoneBook phoneBook = new PhoneBook();
+            String[] names = new String[]{"Christopher", "Chris", "Christian", "Christina"};
+            for (int i = 0; i < names.length; i++) {
+                String name = names[i];
+                phoneBook.add(name, "");
+            }
 
-        // then
-        Assert.assertEquals(Arrays.asList(names), actualNames);
-    }
+            // when
+            List<String> actualNames = phoneBook.getAllContactNames();
 
-    @Test
-    public void test2() {
-        // given
-        PhoneBook phoneBook = new PhoneBook();
-        String[] names = new String[]{"Chris", "Christian", "Christopher", "Christina"};
-        for (int i = 0; i < names.length; i++) {
-            String name = names[i];
-            phoneBook.add(name, "");
+            // then
+            Assert.assertEquals(Arrays.asList(names), actualNames);
         }
 
-        // when
-        List<String> actualNames = phoneBook.getAllContactNames();
+        @Test
+        public void test3() {
+            // given
+            PhoneBook phoneBook = new PhoneBook();
+            String[] names = new String[]{"Aaron", "Ashley", "Albert", "Alfred"};
+            for (int i = 0; i < names.length; i++) {
+                String name = names[i];
+                phoneBook.add(name, "");
+            }
 
-        // then
-        Assert.assertEquals(Arrays.asList(names), actualNames);
-    }
+            // when
+            List<String> actualNames = phoneBook.getAllContactNames();
 
-    @Test
-    public void test3() {
-        // given
-        PhoneBook phoneBook = new PhoneBook();
-        String[] names = new String[]{"Ashley", "Aaron", "Albert", "Alfred"};
-        for (int i = 0; i < names.length; i++) {
-            String name = names[i];
-            phoneBook.add(name, "");
+            // then
+            Assert.assertEquals(Arrays.asList(names), actualNames);
         }
-
-        // when
-        List<String> actualNames = phoneBook.getAllContactNames();
-
-        // then
-        Assert.assertEquals(Arrays.asList(names), actualNames);
     }
-}
+
